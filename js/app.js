4c54bb98ba2afc4edc2fac80669efe5d86b7fb47
zafiroazul <mchristy.ccb@gmail.com>
2016-11-16 13:45:21 -0500

    añadiendo cambios

--- js/app.js
+++ js/app.js
@@ -2,17 +2,34 @@ $(document).ready(function (){
 	$("#search-form button").click(function(event){
 		event.preventDefault();
 		var term = cleanTerm($("#search-form input").val());
-		var results = callItunesSearch(term, showResults, noResults, error);
+		var results = callItunesSearch(term, showResults);
 	});
 
-	function callItunesSearch(searchTerm, showResults, noResultsMessage, error){
+	function callItunesSearch(searchTerm, showResults){
 		startLoading();
 		// Pueden revisar el API en el link https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
 		var root = 'https://itunes.apple.com/search?';
 		$.ajax({
 		// completa el ajax aquí
-			parseJSON:root,
-			context: document.body,
+			url: root,
+			method: "GET",
+			data: {
+				term: searchTerm,
+				limit: 10,
+			},
+			succes: function(data){
+				console.log("Tudo bem");
+				showResults(JSON.parse(data));
+			},
+			// error: function(data){
+			// 	console.log(data, status);
+			// 	noResultsMessage();
+			// },
+			complete:function(data){
+				stopLoading();
+			},
+        // }).then(function(data){
+        // 	console.log();
         });
 	}
 
@@ -20,17 +37,23 @@ $(document).ready(function (){
 	function showResults(data){
 		console.log(data);
 		data.results.map(function(cancion, index){
+			var audio = new Audio();
+			audio.src = cancion.previewUrl;
+			audio.controls = true;
 			var song  = document.createElement("div");
 			$(song).addClass("song");
-			// genera los elementos de cada canción aquí
+			var image = document.createElement("img");
+			$(image).attr("src", cancion.artworkUrl30);
+			$(song).append("audio");
+			$(song).append("image");
 			$("#search-results").append(song);
 		});
 	}
   
- 	function noResults(){
-       	$("#search-results").html(song);
-    }
-  	function error(){console.log("Error")}
+ 	// function noResults(){
+  //      	$("#search-results").html(song);
+  //   }
+  // 	function error(){console.log("Error")}
 	
 	function cleanTerm(term){
         term = $.trim(term);
